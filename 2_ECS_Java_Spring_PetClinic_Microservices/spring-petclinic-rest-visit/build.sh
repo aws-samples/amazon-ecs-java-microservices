76168d43bb7f038ef8d3fafa867d8f01c431a7ad
Huy Huynh <huynhz@amazon.com>
2017-06-08 17:15:02 -0700

    cleanup 2_

--- 2_ECS_Java_Spring_PetClinic_Microservices/spring-petclinic-rest-visit/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-result=${PWD##*/}
-aws ecr create-repository --repository-name ${result}
-mvn package docker:build -DpushImage -Dmaven.test.skip=true -X
-elbv2 create-target-group --name ${result} --port 8080 --vpc-id vpc-ee5f6d89 --protocol HTTP
